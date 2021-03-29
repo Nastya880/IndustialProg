72b02463cb1b55ea272cf5324b01c615ddcf61f6
Настя Макеева <oidmakeevy@gmail.com>
2021-03-29 21:13:34 +0300

    lab2 - Makeeva Nastya

---
+++ Header.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+string to_str(double val);
+string AnyX(double y);
+string AnyY(double x);
+string EndlessAnswers(double firstArg, double SecondArg, double ThirdArg);
+string Solve(double a, double b, double c, double d, double e, double f, string output);
